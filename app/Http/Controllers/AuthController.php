f3363b07ca95be1c3733bc77a2c3ecef107fbc3e
WDS <lynv110@gmail.com>
2018-05-22 13:36:59 +0700

    Clear code not used

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,14 +25,6 @@ class AuthController extends Controller
     public function login(Request $request){
         $credentials = $request->only(['email', 'password']);
 
-        if (!$token = JWTAuth::attempt($credentials)) {
-            return response([
-                'status' => 'error',
-                'error' => 'invalid.credentials',
-                'msg' => 'Invalid Credentials.'
-            ], 400);
-        }
-
         $token = null;
         try {
             if (!$token = JWTAuth::attempt($credentials)) {
